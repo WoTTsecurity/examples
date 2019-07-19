b57a931cb5c047ef2331d64e21232d9e3e3ca16e
Viktor Petersson <vpetersson@wireload.net>
2019-07-19 08:28:09 +0100

    Tweaks example code

--- paho-mqtt-client/paho-connect-wott.py
+++ paho-mqtt-client/paho-connect-wott.py
@@ -1,15 +1,25 @@
 #!/usr/bin/env python
 
+import os
 import paho.mqtt.client as mqtt
+
+
+MQTT_SERVER = os.getenv('MQTT_SERVER')
+MQTT_PORT = int(os.getenv('MQTT_PORT', 8333))
+MQTT_TOPIC = os.getenv('MQTT_TOPIC', 'wott/temperature')
+
 
 def on_connect(client, obj, flags, rc):
     print("rc: " + str(rc))
 
+
 def on_message(client, obj, msg):
     print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
 
+
 def on_subscribe(client, obj, mid, granted_qos):
     print("Subscribed: " + str(mid) + " " + str(granted_qos))
+
 
 client = mqtt.Client()
 client.on_connect = on_connect
@@ -26,12 +36,12 @@ client.tls_set(
 # disables peer verification
 # client.tls_insecure_set(True)
 
-# Note that you need to map the ServerDeviceID.d.wott.local to the IP 
+# Note that you need to map the ServerDeviceID.d.wott.local to the IP
 # of the Docker container running MQTT server
-# Do this in /etc/hosts  
+# Do this in /etc/hosts
 # The hostname must match the CN (common name) in the certificate.
-client.connect('ServerDeviceID.d.wott.local', 8883, 60)
+client.connect(MQTT_SERVER, MQTT_PORT, 60)
 
-client.subscribe('wott/temperature', 0)
+client.subscribe(MQTT_TOPIC, 0)
 
 client.loop_forever()
